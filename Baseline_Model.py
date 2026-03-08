9acdaaab69285de9f5643192c1ab298d17b980e2
vidhisapru <110246204+vidhisapru@users.noreply.github.com>
2026-03-08 14:39:29 -0500

    Updated Baseline Model
    Updated baseline model with 80/20 train test split + class weight balanced

--- Baseline_Model.py
+++ Baseline_Model.py
@@ -1,8 +1,10 @@
 # imports 
 import pandas as pd 
 from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import classification_report
+
 
 # load dataset
 df = pd.read_csv("/Users/vidhisapru/Downloads/df_with_dummy_final.csv")
@@ -25,29 +27,51 @@ X = df[[
     "race_6.0"
 ]]
 
+# 80/20 split with stratification on outcome
+X_train, X_test, y_train, y_test = train_test_split(
+    X,
+    y,
+    test_size=0.2,
+    random_state=42,
+    stratify=y
+)
+
 # INTERACTIONS
 
-# wealth x age
-X["wealth_age"] = df["WealthIndex"] * df["subject_age"]
+# interaction term train - age
+X_train["wealth_age"] = X_train["WealthIndex"] * X_train["subject_age"]
 
-# wealth x sex
-X["wealth_sex"] = df["WealthIndex"] * df["sex_2.0"]
+# interaction term train - sex
+X_train["wealth_sex"] = X_train["WealthIndex"] * X_train["sex_2.0"]
 
-# wealth x race
-X["wealth_race2"] = df["WealthIndex"] * df["race_2.0"]
-X["wealth_race3"] = df["WealthIndex"] * df["race_3.0"]
-X["wealth_race4"] = df["WealthIndex"] * df["race_4.0"]
-X["wealth_race5"] = df["WealthIndex"] * df["race_5.0"]
-X["wealth_race6"] = df["WealthIndex"] * df["race_6.0"]
+# interaction term train - race
+X_train["wealth_race2"] = X_train["WealthIndex"] * X_train["race_2.0"]
+X_train["wealth_race3"] = X_train["WealthIndex"] * X_train["race_3.0"]
+X_train["wealth_race4"] = X_train["WealthIndex"] * X_train["race_4.0"]
+X_train["wealth_race5"] = X_train["WealthIndex"] * X_train["race_5.0"]
+X_train["wealth_race6"] = X_train["WealthIndex"] * X_train["race_6.0"]
+
+# interaction term test - age 
+X_test["wealth_age"] = X_test["WealthIndex"] * X_test["subject_age"]
+
+# interaction term test - sex
+X_test["wealth_sex"] = X_test["WealthIndex"] * X_test["sex_2.0"]
+
+# interaction term test - race
+X_test["wealth_race2"] = X_test["WealthIndex"] * X_test["race_2.0"]
+X_test["wealth_race3"] = X_test["WealthIndex"] * X_test["race_3.0"]
+X_test["wealth_race4"] = X_test["WealthIndex"] * X_test["race_4.0"]
+X_test["wealth_race5"] = X_test["WealthIndex"] * X_test["race_5.0"]
+X_test["wealth_race6"] = X_test["WealthIndex"] * X_test["race_6.0"]
 
 # multinomial logistic regression
-model = LogisticRegression(solver="lbfgs", max_iter=3000)
+model = LogisticRegression(solver="lbfgs", max_iter=3000, class_weight="balanced")
 print("Starting model fitting...")
-model.fit(X, y)
+model.fit(X_train, y_train)
 print("model done")
 
 # coeff 
-coef_table = pd.DataFrame(model.coef_, columns=X.columns)
+coef_table = pd.DataFrame(model.coef_, columns=X_train.columns)
 print(coef_table)
 
 # intercepts
@@ -55,11 +79,11 @@ print("Intercepts:")
 print(model.intercept_)
 
 # accuracy 
-print("Training accuracy:", model.score(X,y))
+print("Training accuracy:", model.score(X_test,y_test))
 
 # confusion matrix 
-y_pred = model.predict(X)
-print(confusion_matrix(y, y_pred))
+y_pred = model.predict(X_test)
+print(confusion_matrix(y_test, y_pred))
 
 # precision, recall, F1
-print(classification_report(y, y_pred))
+print(classification_report(y_test, y_pred))
